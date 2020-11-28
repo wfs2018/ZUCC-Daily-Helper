d46d9869084a5ad59e90015a32a9cd2e94bf2d28
cXilicon <xiang20001031@gmail.com>
2020-11-28 15:06:21 +0800

    #TEST 3

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome('/usr/bin/chromedriver')
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('/usr/bin/chromedriver', chrome_options=chrome_options)
 driver.maximize_window()
 driver.get('http://yqdj.zucc.edu.cn/feiyan_api/h5/html/daka/daka.html')
 
